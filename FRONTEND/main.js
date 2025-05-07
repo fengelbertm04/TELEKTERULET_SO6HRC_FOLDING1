e559de66963a45596021ae1aa1bab3a0f5b65d62
Mate <Mate@DESKTOP-AI8GNKK>
2025-05-07 18:53:03 +0200

    matrix input frontend

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -3,9 +3,11 @@ async function GetAreaOutPut()
     
 }
 
-async function SendArea()
+async function SendArea() 
 {
     area = document.getElementById("area").value;
     epsilon = document.getElementById("epsilon").value;
+    const matrix = area.trim().split('\n').map(row => row.split(';').map(cell => parseInt(cell)))
+    console.log(matrix)
 }
 
