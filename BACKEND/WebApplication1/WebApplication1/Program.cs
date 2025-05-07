d4125ebfe8b723faa8bd15c9ec715aca0f5c9130
Mate <Mate@DESKTOP-AI8GNKK>
2025-05-07 18:36:08 +0200

    configuring project.cs

--- BACKEND/WebApplication1/WebApplication1/Program.cs
+++ BACKEND/WebApplication1/WebApplication1/Program.cs
@@ -1,3 +1,5 @@
+using TelekAPI.Data;
+
 namespace TelekAPI
 {
     public class Program
@@ -5,11 +7,20 @@ namespace TelekAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSingleton<IAreaRepository, AreaRepository>();
+
             var app = builder.Build();
+
 
             app.MapGet("/", () => "Hello World!");
 
+            app.UseCors(x => x
+    .AllowCredentials()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithOrigins("http://127.0.0.1:5500", "http://localhost:5500"));
             app.Run();
+
         }
     }
 }
