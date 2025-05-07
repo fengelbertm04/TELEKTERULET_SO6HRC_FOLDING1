b38283297dd3363600463aaf9444f2c129070e9e
Mate <Mate@DESKTOP-AI8GNKK>
2025-05-07 18:39:57 +0200

    setting up frontend

---
+++ FRONTEND/main.js
@@ -0,0 +1,11 @@
+async function GetAreaOutPut()
+{
+    
+}
+
+async function SendArea()
+{
+    area = document.getElementById("area").value;
+    epsilon = document.getElementById("epsilon").value;
+}
+
