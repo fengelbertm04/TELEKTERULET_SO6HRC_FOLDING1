94a01677f39f2bcc32f2551f78c4c477b1ad2797
Berti <Berti@DESKTOP-V8G69DT>
2025-05-09 23:01:10 +0200

    table based on matrix

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -1,13 +1,25 @@
-async function GetAreaOutPut()
-{
+async function GetAreaOutPut() {
     const response = await fetch('http://localhost:5156/area')
     areares = await response.json()
     console.log(areares);
+    
+    const container = document.getElementById('matrix-container');
+    const table = document.createElement('table');
 
+    areares.area.plotArea.forEach(row => {
+        const tr = document.createElement('tr');
+        row.forEach(cell => {
+            const td = document.createElement('td');
+            td.textContent = cell;
+            tr.appendChild(td);
+        });
+        table.appendChild(tr);
+    });
+
+    container.appendChild(table);
 }
 
-async function SendArea() 
-{
+async function SendArea() {
     area = document.getElementById("area").value;
     epsilon = document.getElementById("epsilon").value;
     const matrix = area.trim().split('\n').map(row => row.split(';').map(cell => parseInt(cell)))
