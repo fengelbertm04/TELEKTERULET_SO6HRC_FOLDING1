6a81adabdb72dc62b3dd3f7378bc049b83926efd
Berti <Berti@DESKTOP-V8G69DT>
2025-05-09 23:17:51 +0200

    coloring start

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -2,22 +2,34 @@ async function GetAreaOutPut() {
     const response = await fetch('http://localhost:5156/area')
     areares = await response.json()
     console.log(areares);
-    
+
     const container = document.getElementById('matrix-container');
     const table = document.createElement('table');
 
-    areares.area.plotArea.forEach(row => {
-        const tr = document.createElement('tr');
-        row.forEach(cell => {
-            const td = document.createElement('td');
-            td.textContent = cell;
-            tr.appendChild(td);
-        });
-        table.appendChild(tr);
-    });
+    let cellIndex = 0;
 
+    for (let i = 0; i <areares.area.plotArea.length; i++) {
+        const tr = document.createElement('tr');
+    
+        for (let j = 0; j < areares.area.plotArea[i].length; j++) {
+          const td = document.createElement('td');
+          td.textContent = areares.area.plotArea[i][j]
+    
+          if (i >= areares.startIndex.X && i <= areares.endIndex.X && j >= areares.startIndex.Y && j <= areares.endIndex.Y) {
+            td.classList.add('highlight');
+          }
+    
+          tr.appendChild(td);
+        }
+    
+        table.appendChild(tr);
+      }
+    
     container.appendChild(table);
+
 }
+
+
 
 async function SendArea() {
     area = document.getElementById("area").value;
